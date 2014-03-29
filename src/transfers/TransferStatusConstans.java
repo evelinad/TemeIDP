3cdf33e6e9235f46138e41df594b1f6a30ed24ea
evelinad <evelina.a.dumitrescu@gmail.com>
2014-03-29 07:07:18 -0400

    row added

---
+++ src/transfers/TransferStatusConstans.java
@@ -0,0 +1,10 @@
+package transfers;
+
+public interface TransferStatusConstans {
+	public static final int  STARTED = 0;
+	public static final int DOWNLOADING = 1;
+	public static final int PAUSED = 2;
+	public static final int STOPPED = 3;	
+	
+
+}
