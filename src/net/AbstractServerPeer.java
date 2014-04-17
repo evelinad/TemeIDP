8948bc0ba525089ec194826a6c0655c1799062b0
evelinad <evelina.a.dumitrescu@gmail.com>
2014-04-17 13:54:31 -0400

    forgot to ass abstract classes

---
+++ src/net/AbstractServerPeer.java
@@ -0,0 +1,12 @@
+package net;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+
+public abstract class AbstractServerPeer{
+	
+	protected abstract void acceptOP(SelectionKey key, Selector selector);
+	protected abstract void readOP(SelectionKey key);
+	protected abstract void writeOP(SelectionKey key);	
+}
